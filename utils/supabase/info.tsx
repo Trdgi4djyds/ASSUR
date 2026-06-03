aa0c6a5def573942b27700aecd37e642bb61907e
abrahamsetondji02-cell <abrahamsetondji02@gmail.com>
2026-06-04 00:54:43 +0100

    Update supabaseUrl to Cloudflare tunnel URL

--- utils/supabase/info.tsx
+++ utils/supabase/info.tsx
@@ -1,3 +1,4 @@
 export const projectId = "selfhosted";
-export const supabaseUrl = "http://supabasekong-qwx5j3qfalx0jsygpvlj27x6.194.28.99.132.sslip.io";
+export const supabaseUrl = "https://supabase-tunnel.trycloudflare.com";
+
 export const publicAnonKey = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdXBhYmFzZSIsImlhdCI6MTc4MDUxNjM4MCwiZXhwIjo0OTM2MTg5OTgwLCJyb2xlIjoiYW5vbiJ9.bWJGFzxSWn0iAn9Kg3ewJ9MyCCqiQSaB7xs7PIJp4zU";
